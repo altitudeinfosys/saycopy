a37746bbedcfa4528b724df60d828914aafa6cd6
Tarek Alaaddin <tarek@altitudeinfosys.com>
2026-07-11 23:26:56 -0500

    Rename app to SayCopy

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: 'jest-expo',
   setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
   testMatch: ['**/*.test.ts?(x)'],
+  testPathIgnorePatterns: ['<rootDir>/.worktrees/'],
 };
